9b866d268448f7d85ba7adc8f26f7047045ee8bb
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2022-06-20 21:35:56 -0400

    getElementsByTagName practice

--- script.js
+++ script.js
@@ -44,3 +44,7 @@ document.querySelector('.header')
 const allSelections = document.querySelectorAll('.section')
 
 console.log(allSelections)
+
+document.getElementById('section--1')
+const allButtons = document.getElementsByTagName('button')
+console.log(allButtons)
