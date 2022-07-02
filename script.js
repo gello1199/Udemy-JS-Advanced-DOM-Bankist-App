4197eff253b1c745c4662a7cbea2c73b3a4acf95
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2022-07-02 07:53:07 -0400

    started event propogation practice

--- script.js
+++ script.js
@@ -170,3 +170,18 @@ setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000)
 //   alert('onmouseenter: Great! You are reading the heading')
 // }
 
+// rgb(255,255,255)
+const randomInt = (min, max) => Math.floor(Math.random() * (max - min + 1) + min) 
+const randomColor = () => `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`
+
+document.querySelector('.nav__link').addEventListener('click', function(e) {
+ this.style.backgroundColor = randomColor()
+})
+
+document.querySelector('.nav__links').addEventListener('click', function(e) {
+  
+})
+
+document.querySelector('.nav').addEventListener('click', function(e) {
+  
+})
