ac0a374a85661af62bbcad41e4baac9b0278b0fe
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2022-07-05 11:06:45 -0400

    currentTarget and stopPropogation notes

--- script.js
+++ script.js
@@ -176,12 +176,19 @@ const randomColor = () => `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${rando
 
 document.querySelector('.nav__link').addEventListener('click', function(e) {
  this.style.backgroundColor = randomColor()
+ console.log('LINK', e.target, e.currentTarget)
+ console.log(e.currentTarget === this)
+
+ // stop event propogation
+//  e.stopPropagation()
 })
 
 document.querySelector('.nav__links').addEventListener('click', function(e) {
   this.style.backgroundColor = randomColor()
+  console.log('CONTAINER', e.target, e.currentTarget)
 })
 
 document.querySelector('.nav').addEventListener('click', function(e) {
-  
+  this.style.backgroundColor = randomColor()
+  console.log('NAV', e.target, e.currentTarget)
 })
